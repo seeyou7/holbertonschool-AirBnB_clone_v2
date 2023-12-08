e6bf593c3a93eebe2246d780f183c5999d6be4aa
seeyou7 <>
2023-12-08 09:11:19 -0800

    create review and update import in related file

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,9 @@
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """table of the class amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
