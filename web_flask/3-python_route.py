5f58d46955223140917cd753089b169fddef604b
syou7 <y.saber@outlook.fr>
2023-12-15 16:19:24 +0100

    Defines a new route python and python/text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Start a Flask web application on localhost"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display a message when making a request to root"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display message HBNB!"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_C(text):
+    """Display “C ” followed by the value of the text variable """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python',  strict_slashes=False)
+@app.route('/python/<text>',  strict_slashes=False)
+def python(text="is cool"):
+    """ display “Python ”, followed by the value of the text var """
+    return "python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    """run app flask"""
+    app.run(host='0.0.0.0', port=5000)
