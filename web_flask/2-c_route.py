9b6bb54f45b6f4aa8027f8458ed1eb814716fe19
syou7 <y.saber@outlook.fr>
2023-12-15 12:21:33 +0100

    fix pycode style

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,8 +18,8 @@ def hbnb():
     """Display message HBNB!"""
     return 'HBNB'
 
-@app.route('/c/<text>', stricte_slashes=False)
-def text_C(text)
+@app.route('/c/<text>', strict_slashes=False)
+def text_C(text):
     """Display “C ” followed by the value of the text variable """
     text = text.replace('_', ' ')
     return "C {}".format(text)
