f74be3a222b1ddce9badf2cbdc115f0e570d26d6
seeyou7 <5764@holbertonstudents.com>
2023-12-15 12:11:21 +0100

    Defines a new route /c/var in the Flask application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Start a Flask web application on localhost"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display a message when making a request to root"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display message HBNB!"""
+    return 'HBNB'
+
+@app.route('/c/<text>', stricte_slashes=False)
+def text_C(text)
+    """Display “C ” followed by the value of the text variable """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    """run app flask"""
+    app.run(host='0.0.0.0', port=5000)
