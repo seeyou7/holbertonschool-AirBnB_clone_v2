9ecf14ec7bc86f1d66fffb469a4a5e1797ec7107
syou7 <y.saber@outlook.fr>
2023-12-15 10:25:26 +0100

    Defines a new route '/hbnb' in the Flask application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start a Flask web application on localhost"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display a message when making a request to root"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display message HBNB!"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    """run app flask"""
+    app.run(host='0.0.0.0', port=5000)
