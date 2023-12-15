5d92cf2294d26e215cc175039195069b0d647616
syou7 <y.saber@outlook.fr>
2023-12-15 16:48:18 +0100

    update all code using dynamic routing

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,17 +19,16 @@ def hbnb():
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def text_C(text):
-    """Display “C ” followed by the value of the text variable """
-    text = text.replace('_', ' ')
-    return "C {}".format(text)
+@app.route('/c/<string:text>', strict_slashes=False)
+def dynamic_text(text=None):
+    """Display “C ” + var value using dynamic routing """
+    return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python',  strict_slashes=False)
-@app.route('/python/<text>',  strict_slashes=False)
-def python(text="is cool"):
-    """ display “Python ”, followed by the value of the text var """
+@app.route('/python/<string:text>',  strict_slashes=False)
+def python_dynamic(text="is cool"):
+    """ display  dynamic“Python ”, followed by the value of the text var """
     return "python {}".format(text.replace('_', ' '))
 
 
