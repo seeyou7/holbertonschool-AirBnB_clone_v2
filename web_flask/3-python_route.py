747f265b62ba8a4ec44e22a1aa0873211b1f074d
syou7 <y.saber@outlook.fr>
2023-12-15 16:53:17 +0100

    fix all indentation errors

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Start a Flask web application on localhost"""
-
+"""Start a flask web application on localhost"""
 
 from flask import Flask
 
@@ -9,29 +8,28 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Display a message when making a request to root"""
+    """Display Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Display message HBNB!"""
-    return 'HBNB'
+def HBNB():
+    """Display HBNB!"""
+    return 'HBNB!'
 
 
 @app.route('/c/<string:text>', strict_slashes=False)
 def dynamic_text(text=None):
-    """Display “C ” + var value using dynamic routing """
+    """dynamic routing"""
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python',  strict_slashes=False)
-@app.route('/python/<string:text>',  strict_slashes=False)
-def python_dynamic(text="is cool"):
-    """ display  dynamic“Python ”, followed by the value of the text var """
-    return "python {}".format(text.replace('_', ' '))
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_dynamic(text='is_cool'):
+    """dynamic w/ defaults"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    """run app flask"""
     app.run(host='0.0.0.0', port=5000)
